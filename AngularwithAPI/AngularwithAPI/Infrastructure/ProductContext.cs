a57333267c22535660804b4c12530100fa53ec93
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-05-07 20:47:07 +0530

    commited the api part in c#

---
+++ AngularwithAPI/AngularwithAPI/Infrastructure/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using AngularwithAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularwithAPI.Infrastructure
+{
+    public class ProductContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+    }
+}
