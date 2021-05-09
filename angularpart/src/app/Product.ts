130b1c0018858548a8975c2c69afe810c81bac40
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-05-09 23:05:13 +0530

    c

--- angularpart/src/app/Product.ts
+++
@@ -1,9 +0,0 @@
-export interface products{
-    id : number;
-    title : string;
-    price : number;
-    quantity : number;
-    colour : string;
-    inStock : boolean;
-    expiryDate: string;
-}
--- angularpart/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apibaseurl:"https://localhost:44342/api/products"
-};
